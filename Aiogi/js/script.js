70d01807aa8ee6d24e40838f07fadd111edf4fa5
vitalia-kontanistova <v.konti@outlook.com>
2020-10-06 21:12:47 +0300

    Refactoring

--- Aiogi/js/script.js
+++ Aiogi/js/script.js
@@ -3,12 +3,10 @@ $(document).ready(function () {
     $(".header__burger,.header__menu,.header__profile").toggleClass("active");
     $("body").toggleClass("lock");
   });
-  // проверить при любой ширине окна
   $(".search__input").focus(function (event) {
     $(".header__logo").addClass("hidden");
   });
 
-  // $(".header__list").click(function (event) {
   $(".header__menu").click(function (event) {
     $(".header__burger,.header__menu,.header__profile").removeClass("active");
     $("body").removeClass("lock");
